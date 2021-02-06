196f0b6385e6fc349dc7d0649ba8e84f046b8a0a
Stella Dare <stelladare2@gmail.com>
2021-02-06 14:20:18 +0000

    Java Programming: Day 1, Introduction

---
+++ java-programming/Day-1/Introduction/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double n = (int)(Math.random()*10)+1;
+        System.out.println(""+n);
+    }
+}
