3ee73779d910b7f02e41c3adcf123a34ba6fa438
Stella Dare <stelladare2@gmail.com>
2021-02-07 15:28:12 +0000

    Java Programming, Day 1, java-packaging-and-deployment

---
+++ java-programming/Day-1/java-packaging-and-deployment/classes/course/Hello.java
@@ -0,0 +1,9 @@
+package course;
+import course.demo.AnotherClass;
+ 
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        AnotherClass.display();
+    }
+}
---
+++ java-programming/Day-1/java-packaging-and-deployment/classes/course/demo/AnotherClass.java
@@ -0,0 +1,7 @@
+package course.demo;
+ 
+public class AnotherClass {
+    public static void display() {
+        System.out.println("AnotherClass Display method");
+    }
+}
